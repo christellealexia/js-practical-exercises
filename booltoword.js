a2252506a5d27359c0984dc6d1c67c7e7d58160b
christellealexia <christellealexia53@gmail.com>
2024-04-05 11:46:30 +0200

    Exercises on js fundamentals

---
+++ booltoword.js
@@ -0,0 +1,12 @@
+function boolToWord( bool ){
+  
+     
+      if (bool == false){
+       return "No";
+       }
+     else  
+       return "Yes";
+     
+   };
+   let bool = false;
+   console.log(boolToWord(bool));
