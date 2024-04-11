b9293f587e1fd186c8320eb5136f66a0559a7d45
christellealexia <christellealexia53@gmail.com>
2024-04-11 20:04:14 +0200

    Js exercies

---
+++ BoollToString.js
@@ -0,0 +1,5 @@
+function booleanToString(b) {
+    let s2 = b.toString();
+    return s2;
+
+}
--- sortproducts.js
+++ sortproducts.js
@@ -1,31 +1,29 @@
-function sortProducts(arr){
-    let products = [
-        {
-        id:1,
-        prodName:'tomatoes',
-        prodPrice:2500,
-        },
-        {
-            id:1,
-        prodName:'Onions',
-        prodPrice:500, 
-        },
-        {
-            id:1,
-            prodName:'avocadoes',
-            prodPrice:300,
-            },
-            {
-                id:1,
-                prodName:'carrots',
-                prodPrice:2000,
-                }
-            ]
-           
-                for (let i in products){
-                
-                }
-                }
+function sortProducts(arr) {
 
-    ]
+    let minPrice = arr.sort((a, b) => a.prodPrice - b.prodPrice);
+    return minPrice;
+
 }
+
+let arr = [{
+        id: 1,
+        prodName: 'tomatoes',
+        prodPrice: 2500,
+    },
+    {
+        id: 1,
+        prodName: 'Onions',
+        prodPrice: 500,
+    },
+    {
+        id: 1,
+        prodName: 'avocadoes',
+        prodPrice: 300,
+    },
+    {
+        id: 1,
+        prodName: 'carrots',
+        prodPrice: 2000,
+    }
+];
+console.log(sortProducts(arr));
